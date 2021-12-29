9fc1507eb1c6dec3bb11c1939c42307f946495ce
zara200 <zaraRana41@gmail.com>
2021-12-29 16:25:55 +0500

    test

---
+++ src/main/java/employee.java
@@ -0,0 +1,9 @@
+public class employee {
+
+    float salary=40000;
+    int bonus=345678;
+    public void example(){
+        System.out.println("This is my salary : "+ salary);
+        System.out.println("This is my bonus : "+ bonus );
+    }
+}
---
+++ src/main/java/programmer.java
@@ -0,0 +1,9 @@
+public class programmer extends employee {
+
+    public static void main(String args[]){
+        programmer p=new programmer();
+        p.example();
+//        System.out.println("Programmer salary is:"+p.salary);
+//        System.out.println("Bonus of Programmer is:"+p.bonus);
+    }
+}
